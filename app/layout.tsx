a74b690ec20d4484eec5649a8ae35b0b51d04dd0
avaice <dev@cho-ice.xyz>
2023-09-17 10:25:35 +0900

    refactor: Remove initial files and markup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css'
 import './reset.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import React from 'react'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body>{children}</body>
     </html>
   )
 }
