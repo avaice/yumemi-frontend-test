fcca42def3eeaf3a43d1f0e3f457b22289317416
avaice <dev@cho-ice.xyz>
2023-09-17 22:02:45 +0900

    fix design

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import { PopulationChartContainer } from './_components/PopulationChartContainer
 import { PrefecturesSelector } from './_components/PrefecturesSelector'
 import { getPrefectures } from './_query/server/getPrefectures'
 
+import styles from './page.module.css'
+
 export default async function Home() {
   const prefectures = await getPrefectures()
 
   return (
     <main>
-      <h1>都道府県別の総人口推移</h1>
+      <h1 className={styles.title}>都道府県別の総人口推移</h1>
       <PrefecturesSelector prefectures={prefectures.result} />
       <PopulationChartContainer />
     </main>
