3368836443d6249d315cbca57e2d21242c01ff9b
あいす <80768507+avaice@users.noreply.github.com>
2023-09-18 10:34:55 +0900

    chore: Modify directory structure (#7)

--- app/_query/server/getPrefectures.ts
+++ app/_query/server/getPrefectures.ts
@@ -1,4 +1,4 @@
-import { getFromResasApi } from '@/app/api/_utils/getFromResasApi'
+import { getFromResasApi } from '@/app/_utils/getFromResasApi'
 import { z } from 'zod'
 
 const prefecturesSchema = z.array(
