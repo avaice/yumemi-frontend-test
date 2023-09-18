fc597448a7bae170384204c17d1fd71ae4c1898f
avaice <dev@cho-ice.xyz>
2023-09-18 13:38:31 +0900

    change headers

--- app/api/population/route.ts
+++ app/api/population/route.ts
@@ -40,10 +40,9 @@ export async function GET(request: Request) {
     createResponse({ status: 'ok', data: populationData }),
     {
       headers: {
-        'Cache-Control': 'max-age=180',
-        'CDN-Cache-Control': 'max-age=600',
-        'Vercel-CDN-Cache-Control':
-          'max-age=604800, stale-while-revalidate=3600',
+        'Cache-Control': 'max-age=10',
+        'CDN-Cache-Control': 'max-age=60',
+        'Vercel-CDN-Cache-Control': 'max-age=3600',
       },
     }
   )
