aca72260595a4d9fda631c5aab876026afae637a
avaice <dev@cho-ice.xyz>
2023-09-20 18:14:12 +0900

    implement logic

--- app/_utils/atoms.ts
+++ app/_utils/atoms.ts
@@ -2,3 +2,4 @@ import { atom } from 'jotai'
 import { Prefectures } from '../_query/server/getPrefectures'
 
 export const selectedPrefecturesAtom = atom<Prefectures>([])
+export const isLoadingPopulationsAtom = atom<boolean>(false)
