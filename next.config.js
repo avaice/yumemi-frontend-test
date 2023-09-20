3bba40597c97f6c245aa31f7e92d4c10b7304062
あいす <80768507+avaice@users.noreply.github.com>
2023-09-20 17:00:38 +0900

    chore: Update Dockerfile and deployment directory (#12)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const SUB_DIRECTORY = "/population";
+
+const isProd = process.env.NODE_ENV == "production"
+
+module.exports = {
+    basePath: isProd ? SUB_DIRECTORY : "",
+}
